333735768e3b890932053d57e4332fdb41e77894
vuwwit <vuwwit@gmail.com>
2021-01-12 18:08:05 +1300

    Added a base console application

---
+++ PlayCardGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
